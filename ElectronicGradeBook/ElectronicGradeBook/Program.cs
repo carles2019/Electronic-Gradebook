057a0a089e77fb189eb4d7a8fa1d82873f763813
Joseph Chitsungo <50632784+carles2019@users.noreply.github.com>
2021-01-28 20:26:28 +0100

    Add files via upload

---
+++ ElectronicGradeBook/ElectronicGradeBook/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace ElectronicGradeBook
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("REQUIREMENT:\n" +
+                "\tAn electronic grade book to read grades of an individual student:\n" +
+                "\t1.	Show options to save the grade book in memory(InMemoryBook) or in a file(DiskBook)\n" +
+                "\t2.	Then compute some simple statistics from the scores\n" +
+                "\t3.	The grades must be floating point numbers from 0 to 100\n" +
+                "\t4.	Statistics should show the highest grade, lowest grade, and the average grade\n" +
+                "***************************************************************************************\n");
+            Console.Write("Enter Name: ");
+            var bookName = Console.ReadLine();
+            Book book = null;
+            int saveType;
+            Console.Write("Enter 1 to save grades in memory or 2 to save grades to a file: ");
+            while (true)
+            {
+                try
+                {
+                    saveType = int.Parse(Console.ReadLine());
+                    if (saveType == 1)
+                    {
+                        book = new InMemoryBook(bookName);
+                        break;
+                    }
+                    else if (saveType == 2)
+                    {
+                        book = new DiskBook(bookName);
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("Enter 1 to save grades in memory or 2 to save grades to a file: ");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.Write("Enter 1 to save grades in memory or 2 to save grades to a file: ");
+                }
+            }
+            book.GradeAdded += OnGradeAdded;
+            EnterGrade(book);
+            var stats = book.GetStatistics();
+            Console.WriteLine("\nBook Information:\n");
+            Console.WriteLine("\tBook Name: " + book.Name + "\n");
+            Console.WriteLine($"\tThe lowest grade: {stats.Low}");
+            Console.WriteLine($"\tThe highest grade: {stats.High}");
+            Console.WriteLine($"\tThe average grade: {stats.Average}");
+            Console.WriteLine($"\tThe letter grade: {stats.Letter}");
+            Console.ReadLine();
+        }
+        private static void EnterGrade(Book book)
+        {
+            while (true)
+            {
+                Console.Write("\nEnter a grade or 'q' to quit:");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added.");
+        }
+    }
+}
